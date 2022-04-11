5aa2653b4a65e02989338ee86c29b80a5cc86313
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-04-11 19:47:48 +0200

    Finish task

--- Server.mjs
+++ Server.mjs
@@ -1,37 +1,62 @@
 import {createServer} from 'http';
 import {UsersServices} from './services/usersServices.mjs'
 import fs from "fs";
+import * as url from "url";
+import * as querystring from "querystring";
 
 export class Server {
     static run() {
-
         createServer(((request, response) => {
             if (request.url === '/' || request.url === '/home') {
                 fs.createReadStream('./views/home.html').pipe(response);
             } else if (request.url === '/login' && request.method === "POST") {
                 let body = [];
-
                 request.on('data', (chunk) => {
                     body.push(chunk);
                 }).on('end', () => {
                     body = Buffer.concat(body).toString();
                     let result = new UsersServices().login(JSON.parse(body));
+                    if (result.success)
+                        response.writeHead(200, {'Content-Type': 'application/json'});
+                    else {
+                        response.writeHead(400, {'Content-Type': 'application/json'})
+                    }
                     response.end(JSON.stringify(result));
                 });
-            } else if (request.url === '/signup') {
-                response.writeHead(200, {'Content-Type': 'application/json'});
-                var obj = {
-                    firstname: 'John',
-                    lastname: 'Doe'
-                };
-                response.end(JSON.stringify(obj));
-            } else if (request.url === '/profile') {
-                response.writeHead(200, {'Content-Type': 'application/json'});
-                var obj = {
-                    firstname: 'John',
-                    lastname: 'Doe'
-                };
-                response.end(JSON.stringify(obj));
+            } else if (request.url === '/signup' && request.method === "POST") {
+                let body = [];
+                request.on('data', (chunk) => {
+                    body.push(chunk);
+                }).on('end', () => {
+                    body = Buffer.concat(body).toString();
+                    let result = new UsersServices().signup(JSON.parse(body));
+                    if (result.success)
+                        response.writeHead(200, {'Content-Type': 'application/json'});
+                    else {
+                        response.writeHead(400, {'Content-Type': 'application/json'})
+                    }
+                    response.end(JSON.stringify(result));
+                });
+            } else if (request.url === '/profile' && request.method === "POST") {
+                let body = [];
+                request.on('data', (chunk) => {
+                    body.push(chunk);
+                }).on('end', () => {
+                    body = Buffer.concat(body).toString();
+                    let result = new UsersServices().getUserByName(JSON.parse(body).username);
+                    if (result !== undefined) {
+                        let html = fs.readFileSync('./views/profile.html', 'utf8');
+                        html = html.replace('UserName', JSON.parse(body).username);
+                        response.writeHead(200, {'Content-Type': 'text/html'});
+                        response.end(html);
+
+                    } else {
+                        let html = fs.readFileSync('./views/profile.html', 'utf8');
+                        html = html.replace('UserName', "Notfound");
+                        response.writeHead(400, {'Content-Type': 'text/html'});
+                        response.end(html);
+                    }
+                });
             } else {
                 response.writeHead(404);
                 response.end();
--- services/usersServices.mjs
+++ services/usersServices.mjs
@@ -1,7 +1,22 @@
+import {FileHandler} from '../utilities/FileHandler.mjs'
+
 export class UsersServices {
 
+    getAllUsers() {
+        return new FileHandler().readJsonFile('./users.json');
+    }
+
+    getUserByName(username) {
+        return this.getAllUsers().find((user) => {
+            if (user.username === username)
+                return user;
+        });
+    }
+
     login(user) {
-        if (user.username === 'dafro' & user.password === '12345')
+        let currentUser = this.getUserByName(user.username);
+
+        if (currentUser !== null && user.password === currentUser.password)
             return {
                 'success': true,
                 'message': `Welcome${user.username}`
@@ -11,4 +26,25 @@ export class UsersServices {
             'message': "Wrong username or password"
         };
     }
+
+    signup(newUser) {
+        let users = this.getAllUsers();
+        console.log(this.getUserByName(newUser.username));
+
+        if (this.getUserByName(newUser.username) === undefined) {
+            users.push(newUser);
+            let fileHandler = new FileHandler();
+            fileHandler.fileName = './users.json';
+            fileHandler.writeJsonFile(JSON.stringify(users));
+            return {
+                'success': true,
+                'newUser': newUser
+            }
+        } else
+            return {
+                success: false,
+                message: 'user name already exists'
+            }
+    }
+
 }
