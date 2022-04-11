fef5ac35d1ac2af80e33df3ae90d11cf0c88f066
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-04-11 14:49:10 +0200

    Creating node server

--- Server.mjs
+++ Server.mjs
@@ -1,5 +1,11 @@
-import http from 'http';
+import {createServer} from 'http';
 
-class Server extends http {
-
+export class Server {
+    static run() {
+        createServer(((req, res) => {
+            console.log("Request has been received");
+        })).listen(3000, 'localhost', () => {
+            console.log("Server is started on ", "http://localhost:3000");
+        });
+    }
 }
---
+++ main.mjs
@@ -0,0 +1,3 @@
+import {Server} from "./Server.mjs";
+
+Server.run();
